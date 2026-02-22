c285e9eca0583e51adf0ce2fdad0439b29b83250
Bogdan Tatarintsev <112606931+leadmee@users.noreply.github.com>
2026-02-23 00:42:35 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,28 +1,17 @@
-tasks = []
-task_id_counter = 1
+"""
+Модуль 9. Пользовательский интерфейс.
+
+Обеспечивает взаимодействие пользователя с системой.
+"""
 
 
 def show_menu():
-    print("\n=== Система планирования задач ===")
+    """Показать меню."""
     print("1. Показать задачи")
     print("2. Добавить задачу")
-    print("3. Удалить задачу")
-    print("4. Выход")
+    print("3. Отчёт")
 
-def add_task():
-    global task_id_counter
 
-    title = input("Название задачи: ")
-    deadline = input("Срок выполнения: ")
-    priority = input("Приоритет (1-5): ")
-
-    task = {
-        "id": task_id_counter,
-        "title": title,
-        "deadline": deadline,
-        "priority": priority
-    }
-
-    tasks.append(task)
-    task_id_counter += 1
-    print("Задача добавлена.")
+def start():
+    """Запуск интерфейса."""
+    show_menu()
