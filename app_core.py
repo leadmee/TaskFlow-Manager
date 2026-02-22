879abfd2ed24aad6f597602e47d09dab8288ff81
Bogdan Tatarintsev <112606931+leadmee@users.noreply.github.com>
2026-02-23 00:39:00 +0300

    Update app_core.py
    docblock added

--- app_core.py
+++ app_core.py
@@ -1,1 +1,33 @@
+"""
+Модуль 1. Ядро приложения.
 
+Отвечает за запуск системы, обработку команд пользователя
+и координацию работы остальных модулей.
+"""
+
+from .task_model import Task
+from .finance import calculate_task_cost
+
+
+class AppCore:
+    """Главный класс приложения."""
+
+    def __init__(self):
+        """Инициализация системы."""
+        self.tasks = []
+
+    def add_task(self, title, priority, cost):
+        """
+        Создать новую задачу.
+
+        :param title: название задачи
+        :param priority: приоритет
+        :param cost: стоимость
+        """
+        task = Task(title, priority, cost)
+        self.tasks.append(task)
+
+    def list_tasks(self):
+        """Вывести список задач."""
+        for t in self.tasks:
+            print(t)
