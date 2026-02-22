eaaad62098c8a50984b8d80efe6838feefde03f0
Bogdan Tatarintsev <112606931+leadmee@users.noreply.github.com>
2026-02-23 00:42:12 +0300

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,16 @@
-from .task_model import Task
+"""
+Модуль 3. Планировщик задач.
+
+Отвечает за сортировку задач по приоритету и сроку.
+"""
+
+from core.task_model import Task
+
 
 def schedule_tasks(tasks):
-    print("Планировщик: объединённая версия")
+    """
+    Отсортировать задачи.
+
+    :param tasks: список задач
+    """
     return sorted(tasks, key=lambda t: (t.priority, t.due_date or "9999-12-31"))
