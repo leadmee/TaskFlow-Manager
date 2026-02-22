88652740244a6eb2ef9e4add8cdef4c1707b7537
Bogdan Tatarintsev <112606931+leadmee@users.noreply.github.com>
2026-02-23 00:41:36 +0300

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,5 +0,0 @@
-from .task_model import Task
-
-def schedule_tasks(tasks):
-    print("Планировщик: сортировка по приоритету")
-    return sorted(tasks, key=lambda t: (t.priority, t.due_date or "9999-12-31"))
