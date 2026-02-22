bc9517b210e3c8b4bc62c2602183bb4fbb3438ff
Bogdan Tatarintsev <112606931+leadmee@users.noreply.github.com>
2026-02-23 00:41:54 +0300

    Update history_store.py

--- history_store.py
+++ history_store.py
@@ -1,1 +1,31 @@
+"""
+Модуль 5. Хранение истории.
 
+Фиксирует изменения задач и финансовых данных.
+"""
+
+from datetime import datetime
+
+
+class HistoryStore:
+    """Хранилище истории."""
+
+    def __init__(self):
+        self.records = []
+
+    def add_record(self, action, obj):
+        """
+        Добавить запись в историю.
+
+        :param action: действие
+        :param obj: объект изменения
+        """
+        self.records.append({
+            "time": datetime.now(),
+            "action": action,
+            "object": obj
+        })
+
+    def get_history(self):
+        """Получить всю историю."""
+        return self.records
